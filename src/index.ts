c50c0528dca6c4121f55c1033f9875e7a1eaf11a
Mihael Šinkec <sinkec.mihael@gmail.com>
2023-01-21 11:52:25 +0100

    Add missing exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export { Mimc7 } from './hash/mimc7'
-export { BN256, BN256Pairing, FQ, FQ2, FQ6 } from './ec/bn256'
+export {
+    BN256,
+    BN256Pairing,
+    FQ,
+    FQ2,
+    FQ6,
+    G1Point,
+    G2Point,
+    TwistPoint,
+    CurvePoint,
+    LineFuncRes,
+} from './ec/bn256'
+export { G16BN256, Proof, VerifyingKey } from './zk/g16bn256'
