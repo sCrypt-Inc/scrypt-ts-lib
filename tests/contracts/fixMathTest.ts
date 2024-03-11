48868ebfdb91f9ab7290d125d2447092f029d9be
Yusuf Idi Maina <120538505+yusufidimaina9989@users.noreply.github.com>
2024-03-11 09:32:27 +0100

    Add fixMathTest

---
+++ tests/contracts/fixMathTest.ts
@@ -0,0 +1,30 @@
+import { SmartContract, method, assert } from "scrypt-ts";
+import "./fixMath";
+import { FixMath } from "./fixMath";
+
+export class TestFixMath extends SmartContract{
+  static readonly scale: bigint= 18446744073709551616n;
+
+  @method()
+  public testLog() {
+    assert(FixMath.log(TestFixMath.scale) == 0n);
+
+    // log(21.8)
+    let res: bigint = FixMath.log(402139020806868238336n);
+    assert(res == 56851204471023106498n);
+  }
+
+  @method()
+  public testExp() {
+    let exptest1 = FixMath.exp(10n * TestFixMath.scale);
+    assert(exptest1 / TestFixMath.scale == 22026n);
+  }
+}
+
+(async ()=>{
+    await TestFixMath.loadArtifact()
+    let instance = new TestFixMath()
+
+    const deployTx = await instance.deploy(1)
+    console.log('deployTx :',  deployTx.id)
+})()
